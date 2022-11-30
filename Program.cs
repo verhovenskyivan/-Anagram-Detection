54bd4b3b9333ba69764a74bf7ea3a2fff98f0a1b
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-11-30 20:17:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static bool IsAnagram(string test, string original)
+    {
+        return SortLower(test) == SortLower(original);
+    }
+
+    public static string SortLower(string str)
+    {
+        return String.Concat(str.ToLower().OrderBy(c => c));
+    }
+}
